349d13b82228435c46bb72f15b428c23a12d14e7
jshields123 <jackshields@live.co.uk>
2021-03-08 16:03:57 +0000

    added in test for withdraw

--- README.md
+++ README.md
@@ -44,6 +44,10 @@ So I can keep my money safe
 I want to be able to deposit money into an Account
 
 As a user
+So I can see how much money I have
+I want to be able to see my balance
+
+As a user
 SO I can track my money
 I want to be able to see dates on activity
 
@@ -51,9 +55,7 @@ As a user
 So I can use my money
 I want to be able to withdraw my money
 
-As a user
-So I can see how much money I have
-I want to be able to see my balance
+
 
 As a user
 So I can see everything on one summary
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,6 +3,15 @@
 # As a user
 # So I can keep my money safe
 # I want to be able to deposit money into an Account
+#
+# As a user
+# So I can see how much money I have
+# I want to be able to see my balance
+
+# As a user
+# So I can use my money
+# I want to be able to withdraw my money
+#
 
 require 'account'
 
@@ -10,26 +19,36 @@ describe Account do
   before(:each) do
     @account = Account.new
   end
+  describe '#balance' do
 
-  it 'has a default balance of zero when asked for balance with no deposits' do
-    expect(@account.balance).to eq 0
+    it 'has a default balance of zero when asked for balance with no deposits' do
+      expect(@account.balance).to eq 0
+    end
   end
 
   describe '#deposit' do
     it 'takes in 100 and adds it to the account balance' do
-      @account.deposit(100)
+        @account.deposit(100)
+        expect(@account.balance).to eq 100
+      end
+
+      it 'takes in 200 and adds it to the account balance' do
+        @account.deposit(200)
+        expect(@account.balance).to eq 200
+      end
+
+      it 'can take in 200 and then 100 and adds it to the balance' do
+        @account.deposit(200)
+        @account.deposit(100)
+        expect(@account.balance).to eq 300
+      end
+    end
+
+  describe '#withdraw ' do
+    it ' takes 100 off the account balance' do
+      @account.deposit(200)
+      @account.withdraw(100)
       expect(@account.balance).to eq 100
-    end
-
-    it 'takes in 200 and adds it to the account balance' do
-      @account.deposit(200)
-      expect(@account.balance).to eq 200
-    end
-
-    it 'can take in 200 and then 100 and adds it to the balance' do
-      @account.deposit(200)
-      @account.deposit(100)
-      expect(@account.balance).to eq 300
     end
   end
 end
@@ -38,12 +57,4 @@ end
 # As a user
 # SO I can track my money
 # I want to be able to see dates on activity
-#
-# As a user
-# So I can use my money
-# I want to be able to withdraw my money
-#
-# As a user
-# So I can see how much money I have
-# I want to be able to see my balance
 #
