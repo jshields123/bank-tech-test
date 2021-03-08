08dd070fb534dfb20d6341ecbb00da8713731000
jshields123 <jackshields@live.co.uk>
2021-03-08 14:41:49 +0000

    added lib files and spec files and split the user stories between the specs

---
+++ spec/bank_account_spec.rb
@@ -0,0 +1,16 @@
+# As a user
+# So I can keep my money safe
+# I want to be able to deposit money into an Account
+#
+# As a user
+# SO I can track my money
+# I want to be able to see dates on activity
+#
+# As a user
+# So I can use my money
+# I want to be able to withdraw my money
+#
+# As a user
+# So I can see how much money I have
+# I want to be able to see my balance
+#
---
+++ spec/bank_statement_spec.rb
@@ -0,0 +1,3 @@
+# As a user
+# So I can see everything on one summary
+# I would like a bank statement.
