69b5c8e1bc7ff91386e188b2adf0af2475202512
jshields123 <jackshields@live.co.uk>
2021-03-09 10:52:41 +0000

    refactored DATE to constant

--- lib/account.rb
+++ lib/account.rb
@@ -4,23 +4,21 @@ class Account
   attr_reader :balance, :transactions
 
   DEFAULT_BALANCE = 0
+  DATE = Time.new.strftime("%d/%m/%Y")
 
   def initialize
     @balance = DEFAULT_BALANCE
     @transactions = []
-
   end
 
   def deposit(money)
-    date = Time.new.strftime("%d/%m/%Y")
     @balance += money
-    "#{date}, credit, #{money}"
+    "#{DATE}, credit, #{money}"
   end
 
   def withdraw(money)
-    date = Time.new.strftime("%d/%m/%Y")
     @balance -= money
-    "#{date}, debit, #{money}"
+    "#{DATE}, debit, #{money}"
   end
 
 end
