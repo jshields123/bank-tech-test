8fbb54da1c382fe102d423c9721044f1d8b6d3e1
jshields123 <jackshields@live.co.uk>
2021-03-10 08:45:54 +0000

    added in tests for transaction class

---
+++ spec/transaction_spec.rb
@@ -0,0 +1,13 @@
+require 'transaction'
+
+describe Transaction do
+
+  let(:transaction) { Transaction.new("10/03/2021", 100, 0, 100) }
+
+  it 'creates a transaction' do
+    expect(transaction.date).to eq "10/03/2021"
+    expect(transaction.balance).to eq 100
+    expect(transaction.credit).to eq 100
+    expect(transaction.debit).to eq 0
+  end
+end
